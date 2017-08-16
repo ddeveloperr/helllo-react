1eb6c6457c23763ec197600500946abbe9ef0ea5
KemalC <ddeveloperr@gmail.com>
2017-08-16 12:56:39 +0000

    Add Component Properties, add default props value

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,27 @@
+var Greeter = React.createClass({
+    
+    getDefaultProps: function(){
+        return {
+          name: 'React',
+          message: 'This is the default message!'
+        };
+    },
+    render: function() {
+        var name = this.props.name;
+        var message = this.props.message;
+        return(
+            <div>
+              <h1> Hello {name} called from React Component! </h1>
+              <p>{message + "!!!"}</p>
+            </div> 
+            );
+    } 
+});
+
+let firstName = 'Kemal';
+let myMessage = "I'm so happy to learn React.js so fast!";
+
 ReactDOM.render(
-             <h1> Hello React! </h1>,
+             <Greeter name={firstName} message =  {myMessage} />,
              document.getElementById('app')
              );
