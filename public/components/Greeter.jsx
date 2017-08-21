2419af1c6c822f2c95c3a7913aec2c1e38a752c7
KemalC <ddeveloperr@gmail.com>
2017-08-21 07:02:31 +0000

    Refactor Greeter component

--- public/components/Greeter.jsx
+++ public/components/Greeter.jsx
@@ -1,5 +1,38 @@
-function greeter() {
-    document.write('I am "greeder function" from Greeder module! Cool! Unerstend this flow! ');
-}
+var React = require('react'); 
+var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('./GreeterForm'); 
 
-module.exports = greeter;
+
+var Greeter = React.createClass({
+    
+    getDefaultProps: function(){
+        return {
+          name: 'React',
+          message: 'This is the default message!'
+        };
+    },
+    
+    getInitialState: function(){
+      return {
+          name: this.props.name,
+          message: this.props.message
+      };  
+    },
+    
+    handleNewData: function(updates){
+        this.setState(updates); 
+    },
+    
+    render: function() {
+        var name = this.state.name;
+        var message = this.state.message;
+        return(
+            <div>
+              <GreeterMessage name = {name} message = {message}/>
+              <GreeterForm onNewData = {this.handleNewData}/>
+            </div> 
+            );
+    } 
+});
+
+module.exports = Greeter;
