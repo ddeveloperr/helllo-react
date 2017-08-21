76d339047230bfa8175c07a743e231a652ed6324
KemalC <ddeveloperr@gmail.com>
2017-08-21 06:52:07 +0000

    Separate components

--- public/app.jsx
+++ public/app.jsx
@@ -1,19 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var GreeterMessage = React.createClass({
-    render: function(){
-        var name = this.props.name;
-        var message = this.props.message;
-        
-        return(
-            <div>
-              <h1>Hello {name}</h1>
-              <p>{message}</p>
-            </div>
-            );
-    }
-});
+var GreeterMessage = require('./components/GreeterMessage');
 
 var GreeterForm = React.createClass({
     
